4e209ad9a7e5c51d4e6ec88a44e9ad114c523d3f
Nikita Kozelko <nikitakozelko@gmail.com>
2017-11-28 17:32:05 +0300

    Реализован каждый из описанных категорий задач. Добавлена функция поиска по ключевым словам.

--- Code/scripts.js
+++ Code/scripts.js
@@ -1,5 +1,4 @@
 window.onload = function(){
-//    alert("123");
     
     var todoList = [];
     if (localStorage.getItem('todo')!=undefined) {
@@ -25,14 +24,100 @@ window.onload = function(){
     
     document.getElementById('allList').onclick = function() {
         writeNoteDoneList();
+        if (document.getElementById('Tasks').innerHTML == '') {writeEmptyMessage();}  
     }
     
     document.getElementById('doneList').onclick = function() {
         document.getElementById('Tasks').innerHTML = '';
         for (var key in todoList){
             if (todoList[key].check == true){
+                writeDoneItem(key);
+            }
+        }
+        if (document.getElementById('Tasks').innerHTML == '') {writeEmptyMessage();}
+    }
+    
+    document.getElementById('todayList').onclick = function() {
+        document.getElementById('Tasks').innerHTML = '';
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1;
+        var day = now.getDate();
+        var strDateNow = year + "-" + month + "-" + day;
+        for (var key in todoList){
+            if (todoList[key].date.localeCompare(strDateNow) == 0){
                 writeItem(key);
             }
+        }
+    }
+    
+    document.getElementById('next7DayList').onclick = function() {
+        document.getElementById('Tasks').innerHTML = '';
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth();
+        var day = now.getDate();
+        var refNow = new Date(year, month, day, 0, 0, 0, 0);
+        for (var key in todoList){
+            year = todoList[key].date.substring(0, 4);
+            month = todoList[key].date.substring(5, 7) -1;
+            day = todoList[key].date.substring(8);
+            var deadline = new Date(year, month, day, 0, 0, 0, 0);
+            //alert(deadline - now);
+            if ((deadline - refNow < 688248497) && (deadline - refNow >= 0)){
+                writeItem(key);
+            }
+        }
+    }
+    
+    document.getElementById('overdueList').onclick = function() {
+        document.getElementById('Tasks').innerHTML = '';
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth();
+        var day = now.getDate();
+        var refNow = new Date(year, month, day, 0, 0, 0, 0);
+        for (var key in todoList){
+            if (todoList[key].date == "") {continue;}
+            year = todoList[key].date.substring(0, 4);
+            month = todoList[key].date.substring(5, 7) -1;
+            day = todoList[key].date.substring(8);
+            var deadline = new Date(year, month, day, 0, 0, 0, 0);
+            //alert(deadline - now);
+            if (deadline - refNow < 0) {
+                writeItem(key);
+            }
+        }
+    }
+    
+    document.getElementById('forgottenList').onclick = function() {
+        document.getElementById('Tasks').innerHTML = '';
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth();
+        var day = now.getDate();
+        var refNow = new Date(year, month, day, 0, 0, 0, 0);
+        for (var key in todoList){
+            if (todoList[key].date == "") {continue;}
+            year = todoList[key].date.substring(0, 4);
+            month = todoList[key].date.substring(5, 7) -1;
+            day = todoList[key].date.substring(8);
+            var deadline = new Date(year, month, day, 0, 0, 0, 0);
+            //alert(deadline - now);
+            if ((deadline - refNow < -688248497) && (deadline - refNow < 0)){
+                writeItem(key);
+            }
+        }
+    }
+    
+    document.getElementById('searchBtn').onclick = function() {
+        document.getElementById('Tasks').innerHTML = '';
+        var searchName = document.getElementById("searchForm").value;
+        for (var key in todoList){
+            if (todoList[key].todo.indexOf(searchName)>=0)
+                {
+                    writeItem(key);
+                }
         }
     }
     
@@ -49,11 +134,19 @@ window.onload = function(){
     function writeItem(key) {
         var div = document.createElement('div');
         div.className = "Task"+todoList[key].id+" Task";
-        div.innerHTML = "<input type='checkbox' id='checkbox" + todoList[key].id + "'>" + todoList[key].todo + "<span  class='addBtn btn' id='span" + todoList[key].id + "'>Delete</span>";
+        div.innerHTML = "<input type='checkbox' id='checkbox" + todoList[key].id + "'>" + todoList[key].todo + "<span  class='deletebtn' id='span" + todoList[key].id + "'>Delete</span>";
         document.getElementById('Tasks').appendChild(div);
         //document.getElementById('span0').addEventListener("onclick", deleteItem);
         document.getElementById('span'+ todoList[key].id).onclick = deleteItem;
         document.getElementById('checkbox'+ todoList[key].id).onclick = checkItem;
+    }
+    
+    function writeDoneItem(key) {
+        var div = document.createElement('div');
+        div.className = "Task"+todoList[key].id+" Task";
+        div.innerHTML = todoList[key].todo + "<span  class='deletebtn' id='span" + todoList[key].id + "'>Delete</span>";
+        document.getElementById('Tasks').appendChild(div);
+        document.getElementById('span'+ todoList[key].id).onclick = deleteItem;
     }
     
     function checkItem() {
@@ -87,5 +180,12 @@ window.onload = function(){
                 }
         }
         localStorage.setItem('todo', JSON.stringify(todoList));
+    }
+    
+    function writeEmptyMessage() {
+        var div = document.createElement('h2');
+        div.className = "EmptyH2";
+        div.innerHTML = "Oops,<br>Nothing in this list";
+        document.getElementById('Tasks').appendChild(div);
     }
 }
