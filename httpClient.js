4b91aa6b5d0d0fd73d0e82d7905e1b3e56d60863
Nikolai Kolyago <nikolaikolyago@gmail.com>
2018-03-27 16:21:05 +0300

    Http Client [2/2] Implement post(url, contentType, content, successCallback, failCallback) function.
    Signed-off-by: Nikolai Kolyago <nikolaikolyago@gmail.com>

--- httpClient.js
+++ httpClient.js
@@ -1,5 +1,4 @@
 var HttpClient = function () {
-
     this.get = function (url, successCallback, failCallback) {
         var httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = function () {
@@ -11,6 +10,19 @@ var HttpClient = function () {
         };
         httpRequest.open("GET", url, true);
         httpRequest.send();
-    }
+    };
 
+    this.post = function (url, contentType, content, successCallback, failCallback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open("POST", url, true);
+        httpRequest.setRequestHeader("Content-type", contentType);
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4) {
+                if (httpRequest.status == 200) {
+                    successCallback(httpRequest.responseText);
+                } else failCallback(httpRequest.status)
+            }
+        };
+        httpRequest.send(content);
+    }
 };
